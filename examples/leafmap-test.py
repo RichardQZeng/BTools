f07910bba7817f3fa77a5ddd2a4eac39b01b5d23
Richard Zeng <502763+RichardQZeng@users.noreply.github.com>
2023-06-13 20:30:52 -0600

    Refactoring code
    Switch to QWidget; reformat code

--- examples/leafmap-test.py
+++ examples/leafmap-test.py
@@ -6,9 +6,10 @@ import leafmap.foliumap as leafmap
 import geopandas as gpd
 
 from PyQt5 import QtCore, QtGui, QtWidgets, QtWebEngineWidgets
+from PyQt5.QtWidgets import QWidget
 
 
-class Window(QtWidgets.QMainWindow):
+class Window(QWidget):
     def __init__(self):
         super().__init__()
         self.initWindow()
@@ -19,48 +20,44 @@ class Window(QtWidgets.QMainWindow):
         self.buttonUI()
 
     def buttonUI(self):
-        shortPathButton = QtWidgets.QPushButton(self.tr("Button 1"))
-        button2 = QtWidgets.QPushButton(self.tr("Button 2"))
-        button3 = QtWidgets.QPushButton(self.tr("Button 3"))
+        button_1 = QtWidgets.QPushButton(self.tr("Button 1"))
+        button_2 = QtWidgets.QPushButton(self.tr("Button 2"))
+        button_3 = QtWidgets.QPushButton(self.tr("Button 3"))
 
-        shortPathButton.setFixedSize(120, 50)
-        button2.setFixedSize(120, 50)
-        button3.setFixedSize(120, 50)
+        button_1.setFixedSize(120, 50)
+        button_2.setFixedSize(120, 50)
+        button_3.setFixedSize(120, 50)
 
         self.view = QtWebEngineWidgets.QWebEngineView()
         self.view.setContentsMargins(50, 50, 50, 50)
 
         central_widget = QtWidgets.QWidget()
-        self.setCentralWidget(central_widget)
         lay = QtWidgets.QHBoxLayout(central_widget)
 
         button_container = QtWidgets.QWidget()
         vlay = QtWidgets.QVBoxLayout(button_container)
         vlay.setSpacing(20)
         vlay.addStretch()
-        vlay.addWidget(shortPathButton)
-        vlay.addWidget(button2)
-        vlay.addWidget(button3)
+        vlay.addWidget(button_1)
+        vlay.addWidget(button_2)
+        vlay.addWidget(button_3)
         vlay.addStretch()
         lay.addWidget(button_container)
         lay.addWidget(self.view, stretch=1)
+        self.setLayout(lay)
 
         m = leafmap.Map(center=(40, -100), zoom=4)
         m.add_basemap("OpenTopoMap")
         
-        gdf = gpd.read_file(
-    "https://github.com/opengeos/leafmap/raw/master/examples/data/cable_geo.geojson"
-)
+        gdf = gpd.read_file("https://github.com/opengeos/leafmap/raw/master/examples/data/cable_geo.geojson")
         filepath = "https://raw.githubusercontent.com/opengeos/leafmap/master/examples/data/us_cities.csv"
         m.add_gdf(gdf, layer_name="Cable lines") 
-        m.add_heatmap(
-                        filepath,
-                        latitude="latitude",
-                        longitude='longitude',
-                        value="pop_max",
-                        name="Heat map",
-                        radius=20,
-                    )
+        m.add_heatmap(filepath,
+                      latitude="latitude",
+                      longitude='longitude',
+                      value="pop_max",
+                      name="Heat map",
+                      radius=20)
         
         left = 'ESA WorldCover 2021 S2 FCC'
         right = 'ESA WorldCover 2021 S2 TCC'
@@ -70,7 +67,6 @@ class Window(QtWidgets.QMainWindow):
         m.add_text(right, position='bottomright')
 
         self.view.setHtml(m.to_html())
-        
 
 
 if __name__ == "__main__":
