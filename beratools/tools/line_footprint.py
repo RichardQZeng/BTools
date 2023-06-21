5f06a54432ec5328a2e3beab507525cedf444f4a
Richard Zeng <502763+RichardQZeng@users.noreply.github.com>
2023-06-21 17:31:23 -0600

    Update line_footprint.py
    Refactor code

--- beratools/tools/line_footprint.py
+++ beratools/tools/line_footprint.py
@@ -1,8 +1,6 @@
 import time
-import os
 import warnings
-# to suppress panadas UserWarning: Geometry column does not contain geometry when splitting lines
-warnings.simplefilter(action='ignore', category=UserWarning)
+
 import pandas
 import geopandas
 import numpy
@@ -18,12 +16,17 @@ from multiprocessing.pool import Pool
 
 from common import *
 
+# to suppress pandas UserWarning: Geometry column does not contain geometry when splitting lines
+warnings.simplefilter(action='ignore', category=UserWarning)
+
+
 class OperationCancelledException(Exception):
     pass
 
-def line_footprint(callback, in_line, in_canopy, in_cost, corridor_th_value,
-                   max_ln_width, exp_shk_cell, proc_segments, out_footprint, processes, verbose):
-    corridor_th_field='CorridorTh'
+
+def line_footprint(callback, in_line, in_canopy, in_cost, corridor_th_value, max_ln_width,
+                   exp_shk_cell, proc_segments, out_footprint, processes, verbose):
+    corridor_th_field = 'CorridorTh'
     line_seg = geopandas.GeoDataFrame.from_file(in_line)
     max_ln_width = float(max_ln_width)
     exp_shk_cell = int(exp_shk_cell)
@@ -38,52 +41,51 @@ def line_footprint(callback, in_line, in_canopy, in_cost, corridor_th_value,
             print("Line and raster spatial references are not same, please check.")
             exit()
     del raster
-    if not 'OLnFID' in line_seg.columns.array:
-        print(
-            "Cannot find {} column in input line data.\n '{}' column will be created".format('OLnFID', 'OLnFID'))
+    if 'OLnFID' not in line_seg.columns.array:
+        print("Cannot find {} column in input line data.\n '{}' column will be created".format('OLnFID', 'OLnFID'))
         line_seg['OLnFID'] = line_seg.index
 
-    if not 'CorridorTh' in line_seg.columns.array:
-        print(
-            "Cannot find {} column in input line data.\n '{}' column will be created".format('CorridorTh', 'CorridorTh'))
+    if 'CorridorTh' not in line_seg.columns.array:
+        print("Cannot find {} column in input line data.\n '{}' "
+              "column will be created".format('CorridorTh', 'CorridorTh'))
         line_seg['CorridorTh'] = corridor_th_value
     else:
         corridor_th_value = float(9999999)
-    if not 'OLnSEG' in line_seg.columns.array:
-        # print(
-        #     "Cannot find {} column in input line data.\n '{}' column will be created base on input Features ID".format('OLnSEG', 'OLnSEG'))
-        line_seg['OLnSEG']=0
+    if 'OLnSEG' not in line_seg.columns.array:
+        line_seg['OLnSEG'] = 0
+        # print("Cannot find {} column in input line data.\n '{}' column will be created "
+        #      "base on input Features ID".format('OLnSEG', 'OLnSEG'))
 
-    Ori_total_feat=len(line_seg)
+    ori_total_feat = len(line_seg)
 
-    if proc_segments== True:
-        Proc_Seg = True
-        print("Spliting lines into segments...")
+    if proc_segments:
+        print("Splitting lines into segments...")
         line_seg = split_into_segments(line_seg)
-        print("Spliting lines into segments...Done")
+        print("Splitting lines into segments...Done")
     else:
-        Proc_Seg = False
-        line_seg = split_into_Equal_Nth_segments(line_seg)
+        line_seg = split_into_equal_nth_segments(line_seg)
 
-    list_dict_segment_all = line_prepare(callback, line_seg, in_canopy, in_cost, corridor_th_field,
-                                         corridor_th_value, max_ln_width, exp_shk_cell, proc_segments, out_footprint,Ori_total_feat)
+    list_dict_segment_all = line_prepare(callback, line_seg, in_canopy, in_cost, corridor_th_field, corridor_th_value,
+                                         max_ln_width, exp_shk_cell, proc_segments, out_footprint, ori_total_feat)
 
     # pass single line one at a time for footprint
     footprint_list = []
-    # USE_MULTI_PROCESSING=False
+
     if USE_MULTI_PROCESSING:
         footprint_list = execute_multiprocessing(list_dict_segment_all, processes)
     else:
         for row in list_dict_segment_all:
             footprint_list.append(process_single_line(row))
             print("ID:{} is Done".format(row['OLnFID']))
+
     print('Generating shapefile...........')
+    
     results = geopandas.GeoDataFrame(pandas.concat(footprint_list))
     results = results.sort_values(by=['OLnFID', 'OLnSEG'])
     results = results.reset_index(drop=True)
 
     # dissolved polygon group by column 'OLnFID'
-    dissolved_results = results.dissolve(by='OLnFID',as_index=False)
+    dissolved_results = results.dissolve(by='OLnFID', as_index=False)
     dissolved_results = dissolved_results.drop(columns=['OLnSEG'])
     print("Saving output.....")
     dissolved_results.to_file(out_footprint)
@@ -101,6 +103,7 @@ def field_name_list(fc):
         field_list = geopandas.read_file(fc).columns.array
     return field_list
 
+
 def has_field(fc, fi):
     # Check column name
     field_list = field_name_list(fc)
@@ -111,35 +114,37 @@ def has_field(fc, fi):
         shapefile = geopandas.GeoDataFrame.from_file(fc)
         for row in range(0, len(shapefile)):
             shapefile.loc[row, fi] = 3.0
+
         shapefile.to_file(fc)
         print("Warning: There is no field named {} in the input data".format('CorridorTh'))
         print("Field: '{}' is added and default threshold (i.e.3) is adopted".format('CorridorTh'))
         return True
     else:
         print("Warning: There is no field named {} in the input data".format(fi))
-
         return False
 
 
 def process_single_line(dict_segment):
     # this function takes single line to work the line footprint
     # (regardless it process the whole line or individual segment)
-
+    print('Processing line with ID: {}'.format(dict_segment['id']), flush=True)
     in_canopy_r = dict_segment['in_canopy_r']
     in_cost_r = dict_segment['in_cost_r']
     # CorridorTh_field=dict_segment['CorridorTh_field']
     corridor_th_value = dict_segment['corridor_th_value']
+
     try:
         corridor_th_value = float(corridor_th_value)
         if corridor_th_value < 0.0:
             corridor_th_value = 3.0
     except ValueError:
         corridor_th_value = 3.0
+
     max_ln_width = dict_segment['max_ln_width']
     exp_shk_cell = dict_segment['exp_shk_cell']
     # out_footprint=dict_segment['out_footprint']
     shapefile_proj = dict_segment['Proj_crs']
-    orginal_col_name_list = dict_segment['Orgi_col']
+    orginal_col_name_list = dict_segment['orgi_col']
 
     # segment line feature ID
     FID = dict_segment['OLnSEG']
@@ -162,14 +167,14 @@ def process_single_line(dict_segment):
 
     # Create Point "destination"
     destination_point = shapely.Point([x2, y2])
-    destination = [shapes for shapes in
-                   geopandas.GeoDataFrame(geometry=[destination_point], crs=shapefile_proj).geometry]
+    destination = [shapes for shapes in geopandas.GeoDataFrame(geometry=[destination_point],
+                                                               crs=shapefile_proj).geometry]
 
     # Buffer around line and clip cost raster and canopy raster
     # TODO: deal with NODATA
     with rasterio.open(in_cost_r) as src1:
-        clip_in_cost_r, out_transform1 = rasterio.mask.mask(src1, [shapely.buffer(feat, max_ln_width)], crop=True
-                                                            ,nodata=-9999, filled=True)
+        clip_in_cost_r, out_transform1 = rasterio.mask.mask(src1, [shapely.buffer(feat, max_ln_width)],
+                                                            crop=True, nodata=-9999, filled=True)
         out_meta = src1.meta
         crs = src1.crs
         crs_code = src1.meta['crs']
@@ -184,8 +189,8 @@ def process_single_line(dict_segment):
 
     del src1
     with rasterio.open(in_canopy_r) as src:
-        clip_in_canopy_r, out_transform2 = rasterio.mask.mask(src, [shapely.buffer(feat, max_ln_width)], crop=True,nodata=-9999,
-                                                       filled=True)
+        clip_in_canopy_r, out_transform2 = rasterio.mask.mask(src, [shapely.buffer(feat, max_ln_width)],
+                                                              crop=True, nodata=-9999, filled=True)
         out_meta = src.meta
         crs = src.crs
 
@@ -200,35 +205,32 @@ def process_single_line(dict_segment):
         clip_cost_r = numpy.squeeze(clip_in_cost_r, axis=0)
         clip_canopy_r = numpy.squeeze(clip_in_canopy_r, axis=0)
 
-         # Rasterize source point
-        rasterized_source = features.rasterize(origin, out_shape=clip_cost_r.shape
-                                               , transform=out_transform1,
+        # Rasterize source point
+        rasterized_source = features.rasterize(origin, out_shape=clip_cost_r.shape, transform=out_transform1,
                                                out=None, fill=0, all_touched=True, default_value=1, dtype=None)
         source = numpy.transpose(numpy.nonzero(rasterized_source))
 
         # generate the cost raster to source point
         mcp_source = MCP_Geometric(clip_cost_r, sampling=(cell_size_x, cell_size_y))
         source_cost_acc = mcp_source.find_costs(source)[0]
-
         del mcp_source
+
         # Rasterize destination point
-        rasterized_destination = features.rasterize(destination, out_shape=clip_cost_r.shape,
-                                                    transform=out_transform1,
+        rasterized_destination = features.rasterize(destination, out_shape=clip_cost_r.shape, transform=out_transform1,
                                                     out=None, fill=0, all_touched=True, default_value=1, dtype=None)
         destination = numpy.transpose(numpy.nonzero(rasterized_destination))
 
         # generate the cost raster to destination point
         mcp_dest = MCP_Geometric(clip_cost_r, sampling=(cell_size_x, cell_size_y))
         dest_cost_acc = mcp_dest.find_costs(destination)[0]
-
         del mcp_dest
 
         # Generate corridor raster
-        corridor = source_cost_acc+ dest_cost_acc
-        corridor=numpy.ma.masked_invalid(corridor)
+        corridor = source_cost_acc + dest_cost_acc
+        corridor = numpy.ma.masked_invalid(corridor)
 
         # Calculate minimum value of corridor raster
-        if not numpy.ma.min(corridor) is None:
+        if numpy.ma.min(corridor) is not None:
             corr_min = float(numpy.ma.min(corridor))
         else:
             corr_min = 0.05
@@ -236,13 +238,13 @@ def process_single_line(dict_segment):
         # corridor[numpy.isinf(corridor)]=-9999
         # masked_corridor = numpy.ma.masked_where(corridor==-9999, corridor)
         # Set minimum as zero and save minimum file
-        corridor_min = numpy.ma.where((corridor - corr_min)> corridor_th_value, 1.,0.)
+        corridor_min = numpy.ma.where((corridor - corr_min) > corridor_th_value, 1.0, 0.0)
 
         # Process: Stamp CC and Max Line Width
 
         # Original code here
         # RasterClass = SetNull(IsNull(CorridorMin),((CorridorMin) + ((Canopy_Raster) >= 1)) > 0)
-        temp1 = (corridor_min+ clip_canopy_r)
+        temp1 = (corridor_min + clip_canopy_r)
         raster_class = numpy.ma.where(temp1 == 0, 1, 0).data
 
         # BERA proposed Binary morphology
@@ -252,14 +254,14 @@ def process_single_line(dict_segment):
             # Process: Expand
             # FLM original Expand equivalent
             cell_size = int(exp_shk_cell * 2 + 1)
-            expanded = ndimage.grey_dilation(raster_class, size=(cell_size , cell_size))
+            expanded = ndimage.grey_dilation(raster_class, size=(cell_size, cell_size))
 
             # BERA proposed Binary morphology Expand
             # Expanded = ndimage.binary_dilation(RasterClass_binary, iterations=exp_shk_cell,border_value=1)
 
             # Process: Shrink
             # FLM original Shrink equivalent
-            file_shrink = ndimage.grey_erosion(expanded, size=(cell_size , cell_size))
+            file_shrink = ndimage.grey_erosion(expanded, size=(cell_size, cell_size))
 
             # BERA proposed Binary morphology Shrink
             # fileShrink = ndimage.binary_erosion((Expanded),iterations=Exp_Shk_cell,border_value=1)
@@ -272,12 +274,12 @@ def process_single_line(dict_segment):
         clean_raster = ndimage.gaussian_filter(file_shrink, sigma=0, mode='nearest')
 
         # creat mask for non-polygon area
-        mask = numpy.where(clean_raster == 1, True, False)
+        msk = numpy.where(clean_raster == 1, True, False)
 
         # Process: ndarray to shapely Polygon
-        out_polygon = features.shapes(clean_raster, mask=mask, transform=out_transform1)
+        out_polygon = features.shapes(clean_raster, mask=msk, transform=out_transform1)
 
-        # create a shapely multipoly
+        # create a shapely multipolygon
         multi_polygon = []
         for shape, value in out_polygon:
             multi_polygon.append(shapely.geometry.shape(shape))
@@ -287,57 +289,64 @@ def process_single_line(dict_segment):
         out_data = pandas.DataFrame({'OLnFID': [OID], 'OLnSEG': [FID], 'geometry': poly})
         out_gdata = geopandas.GeoDataFrame(out_data, geometry='geometry', crs=shapefile_proj)
 
-
         return out_gdata
 
     except Exception as e:
         print('Exception: {}'.format(e))
+
+
 def split_line_fc(line):
     return list(map(LineString, zip(line.coords[:-1], line.coords[1:])))
 
-def split_line_nPart(line):
+
+def split_line_npart(line):
     # Work out n parts for each line (divided by 30m)
-    n=math.ceil(line.length/30)
-    if n>1:
+    n = math.ceil(line.length/30)
+    if n > 1:
         # divided line into n-1 equal parts;
-        distances=numpy.linspace(0,line.length,n)
-        points=[line.interpolate(distance) for distance in distances]
-        line=shapely.LineString(points)
-        mline=split_line_fc(line)
+        distances = numpy.linspace(0, line.length, n)
+        points = [line.interpolate(dist) for dist in distances]
+        line = shapely.LineString(points)
+        mline = split_line_fc(line)
     else:
-        mline=line
+        mline = line
+
     return mline
+
+
 def split_into_segments(df):
-    odf=df
-    crs=odf.crs
-    if not 'OLnSEG' in odf.columns.array:
+    odf = df
+    crs = odf.crs
+    if 'OLnSEG' not in odf.columns.array:
         df['OLnSEG'] = numpy.nan
 
-    df=odf.assign(geometry=odf.apply(lambda x: split_line_fc(x.geometry), axis=1))
-    df=df.explode()
+    df = odf.assign(geometry=odf.apply(lambda x: split_line_fc(x.geometry), axis=1))
+    df = df.explode()
 
     df['OLnSEG'] = df.groupby('OLnFID').cumcount()
-    gdf=geopandas.GeoDataFrame(df,geometry=df.geometry,crs=crs)
+    gdf = geopandas.GeoDataFrame(df, geometry=df.geometry, crs=crs)
     gdf = gdf.sort_values(by=['OLnFID', 'OLnSEG'])
-    gdf=gdf.reset_index(drop=True)
-    return  gdf
+    gdf = gdf.reset_index(drop=True)
+    return gdf
 
-def split_into_Equal_Nth_segments(df):
-    odf=df
-    crs=odf.crs
-    if not 'OLnSEG' in odf.columns.array:
+
+def split_into_equal_nth_segments(df):
+    odf = df
+    crs = odf.crs
+    if 'OLnSEG' not in odf.columns.array:
         df['OLnSEG'] = numpy.nan
-    df=odf.assign(geometry=odf.apply(lambda x: split_line_nPart(x.geometry), axis=1))
-    df=df.explode()
+    df = odf.assign(geometry=odf.apply(lambda x: split_line_npart(x.geometry), axis=1))
+    df = df.explode()
 
     df['OLnSEG'] = df.groupby('OLnFID').cumcount()
-    gdf=geopandas.GeoDataFrame(df,geometry=df.geometry,crs=crs)
+    gdf = geopandas.GeoDataFrame(df, geometry=df.geometry, crs=crs)
     gdf = gdf.sort_values(by=['OLnFID', 'OLnSEG'])
-    gdf=gdf.reset_index(drop=True)
-    return  gdf
+    gdf = gdf.reset_index(drop=True)
+    return gdf
 
-def line_prepare(callback, line_seg, in_canopy_r, in_cost_r, corridor_th_field,
-                 corridor_th_value, max_ln_width, exp_shk_cell, Proc_Seg, out_footprint,Ori_total_feat):
+
+def line_prepare(callback, line_seg, in_canopy_r, in_cost_r, corridor_th_field, corridor_th_value,
+                 max_ln_width, exp_shk_cell, proc_seg, out_footprint, ori_total_feat):
 
     # get the list of original columns names
     field_list_col = field_name_list(line_seg)
@@ -356,17 +365,18 @@ def line_prepare(callback, line_seg, in_canopy_r, in_cost_r, corridor_th_field,
             # creates a geometry object
             feat = line_seg.loc[row].geometry
 
-            feature_attributes = {}
-            feature_attributes['Seg_leng'] = feat.length
-            feature_attributes['geometry'] = feat
-            feature_attributes['Proj_crs'] = line_seg.crs
+            feature_attributes = {'seg_length': feat.length, 'geometry': feat, 'Proj_crs': line_seg.crs, 'id': i}
+            # feature_attributes['seg_length'] = feat.length
+            # feature_attributes['geometry'] = feat
+            # feature_attributes['Proj_crs'] = line_seg.crs
+            # feature_attributes['id'] = i
 
             for col_name in keep_field_name:
                 feature_attributes[col_name] = line_seg.loc[row, col_name]
             list_of_segment.append(feature_attributes)
-            i = i + 1
+            i += 1
 
-        print("There are {} lines to be processed.".format(Ori_total_feat))
+        print("There are {} lines to be processed.".format(ori_total_feat))
     else:
         print("Input line feature is corrupted, exit!")
         exit()
@@ -379,9 +389,9 @@ def line_prepare(callback, line_seg, in_canopy_r, in_cost_r, corridor_th_field,
         record['corridor_th_value'] = record['CorridorTh']
         record['max_ln_width'] = max_ln_width
         record['exp_shk_cell'] = exp_shk_cell
-        record['proc_seg'] = Proc_Seg
+        record['proc_seg'] = proc_seg
         record['out_footprint'] = out_footprint
-        record['Orgi_col'] = field_list_col
+        record['orgi_col'] = field_list_col
 
     # return list of geodataframe represents each line or segment
     return list_of_segment
@@ -392,15 +402,17 @@ def execute_multiprocessing(line_args, processes):
         total_steps = len(line_args)
         features = []
         with Pool(processes) as pool:
-            chunksize = math.ceil(total_steps / processes)
+            # chunksize = math.ceil(total_steps / processes)
+            # chunk_size = 1000
             step = 0
             # execute tasks in order, process results out of order
-            for result in pool.imap_unordered(process_single_line, line_args,chunksize=chunksize):
+            for result in pool.imap_unordered(process_single_line, line_args):  # , chunksize=chunk_size):
                 if BT_DEBUGGING:
                     print('Got result: {}'.format(result), flush=True)
                 features.append(result)
                 step += 1
-                print('%{}'.format(step / total_steps * 100))
+                print('%{}'.format(step/total_steps*100), flush=True)
+
         return features
     except OperationCancelledException:
         print("Operation cancelled")
