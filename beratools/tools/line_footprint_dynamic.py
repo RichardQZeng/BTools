748e3162588610784382b08bc9c081b59123f1dc
Richard Zeng <502763+RichardQZeng@users.noreply.github.com>
2023-07-13 17:10:36 -0600

    code changes

--- beratools/tools/line_footprint_dynamic.py
+++ beratools/tools/line_footprint_dynamic.py
@@ -18,7 +18,8 @@
 # line_footprint_dynamic.py
 # Script Author: Maverick Fong
 # Date: 2023-May
-# Use open sources python library for produce dynamic footprint from dynamic canopy and cost raster with lines and CHM input.
+# Use open sources python library for produce dynamic footprint
+# from dynamic canopy and cost raster with lines and CHM input.
 # Prerequisite:  Line feature class must have the attribute Fields: "OLnFID" adn CHM raster
 # line_footprint_dynamic.py
 # This script is part of the BERA toolset
