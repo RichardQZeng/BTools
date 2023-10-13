2d8726a847b0d251b9663f368f28a0cad544d5db
Maverick <87385184+Maverick-F@users.noreply.github.com>
2023-10-13 13:21:09 -0600

    Add New function for vegetation coverage in beratool

---
+++ beratools/tools/rpy_vegcoverage.py
@@ -0,0 +1,135 @@
+import os
+import time
+from common import *
+class OperationCancelledException(Exception):
+    pass
+
+try: # integrated R env
+    #check R language within env
+    current_env_path= os.environ['CONDA_PREFIX']
+    # if os.path.isdir(current_env_path):
+    os.environ['R_HOME'] =os.path.join(current_env_path,r"Lib\R")
+    os.environ['R_USER'] = os.path.expanduser('~')
+    os.environ['R_LIBS_USER'] = os.path.join(current_env_path,r"Lib\R\library")
+
+except FileNotFoundError:
+    print("Warning: Please install R for this process!!")
+    exit()
+
+import rpy2.robjects as robjects
+from rpy2.robjects.packages import importr, data
+from rpy2.robjects.vectors import StrVector
+
+def veg_cover_percentage(callback,in_las_folder,out_folder,hmin,hmax,cell_size,processes, verbose):
+
+    r = robjects.r
+    import psutil
+    stats = psutil.virtual_memory()  # returns a named tuple
+    available = getattr(stats, 'available') / 1024000000
+    if 2<processes<=8:
+        if available <= 50:
+            rprocesses = 2
+        elif 50 < available <= 150:
+            rprocesses = 4
+        elif 150 < available <= 250:
+            rprocesses = 8
+    else:
+        rprocesses = 8
+
+    in_las_folder=in_las_folder.replace("\\","/")
+    out_folder=out_folder.replace("\\","/")
+
+    # assign R script file to local variable
+    Beratools_R_script = os.path.join(os.path.dirname(os.path.abspath(__file__)), 'Beratools_r_script.r')
+    # Defining the R script and loading the instance in Python
+    r['source'](Beratools_R_script)
+    # Loading the function defined in R script.
+    r_veg_metrics = robjects.globalenv['veg_cover_percentage']
+    # Invoking the R function
+    r_veg_metrics(in_las_folder,out_folder,hmin,hmax,cell_size,rprocesses)
+
+if __name__ == '__main__':
+    start_time = time.time()
+    print('Finding Vegetation Coverage from Lidar data process\n @ {}'
+          .format(time.strftime("%a, %d %b %Y %H:%M:%S", time.localtime())))
+
+    r=robjects.r
+    utils = importr('utils')
+    base = importr('base')
+    utils.chooseCRANmirror(ind=12) # select the 12th mirror in the list: Canada
+    print("Checking R packages....")
+    CRANpacknames = ['lidR','future','terra'] # ,'comprehenr','na.tools','sf','sp']#,'devtools','gdal']#,'fasterRaster']
+    CRANnames_to_install = [x for x in CRANpacknames if not robjects.packages.isinstalled(x)]
+
+    if len(CRANnames_to_install) > 0:
+       utils.install_packages(StrVector(CRANnames_to_install))
+       packages_found=True
+    else:
+        packages_found= True
+
+    # if packages_found:
+    #    utils.update_packages(checkBuilt = True, ask=False)
+
+    del CRANpacknames,CRANnames_to_install
+
+    print("Checking input parameters....")
+    in_args, in_verbose = check_arguments()
+    in_las_folder=in_args.input["in_las_folder"]
+
+    try:
+        hmin=float(in_args.input["hmin"])
+        if hmin < 0:
+            raise ValueError
+        else:
+            in_args.input["hmin"] = hmin
+    except ValueError:
+        print("Invalid input of minimum height of a tree, default value is used")
+        in_args.input["hmin"] =3.0
+
+    try:
+        hmax=float(in_args.input["hmax"])
+        if hmax<=hmin:
+            raise ValueError
+        else:
+            in_args.input["hmax"]=hmax
+    except ValueError:
+        print("Invalid input of maximum height of a tree, default value is used")
+        if hmin <10:
+            in_args.input["hmax"] =10.0
+        else:
+            in_args.input["hmin"] = 3.0
+            in_args.input["hmax"] = 10.0
+
+    try:
+        cell_size = float(in_args.input["cell_size"])
+        in_args.input["cell_size"] = cell_size
+    except ValueError:
+        print("Invalid input of cell size, default value is used")
+        in_args.input["cell_size"] = 5.0
+
+
+    out_folder=in_args.input["out_folder"]
+
+    if not os.path.exists(in_las_folder):
+        print("Error! Cannot locate Las folder, please check.")
+        exit()
+    else:
+        found = False
+        for files in os.listdir(in_las_folder):
+            if files.endswith(".las") or files.endswith(".laz"):
+                    found=True
+                    break
+        if not found:
+            print("Error! Cannot locate input LAS file(s), please check!")
+            exit()
+
+    if not os.path.exists(out_folder):
+       print("Warning! Cannot locate output folder, It will be created.")
+       os.makedirs(out_folder)
+
+    print("Checking input parameters....Done")
+
+    veg_cover_percentage(print, **in_args.input, processes=int(in_args.processes), verbose=in_verbose)
+
+    print('Finding Vegetation Coverage from Lidar data process is done in {} seconds)'
+          .format(round(time.time() - start_time, 5)))
