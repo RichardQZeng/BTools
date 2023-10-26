8bc6711e5d5b58e76927743401b19521295eac78
Richard Zeng <502763+RichardQZeng@users.noreply.github.com>
2023-10-26 15:27:36 -0600

    Update forest_line_ecosite.py
    Adding polygon functions and debugging attribute issue.

--- beratools/tools/forest_line_ecosite.py
+++ beratools/tools/forest_line_ecosite.py
@@ -3,7 +3,9 @@ from multiprocessing.pool import Pool
 import time
 
 import fiona
+from fiona import Feature, Properties
 from shapely.geometry import shape, mapping
+from shapely.geometry import LineString, MultiLineString, Polygon, MultiPolygon
 from shapely.ops import split
 from shapely import STRtree
 import ray
@@ -34,7 +36,9 @@ def forest_line_ecosite(callback, in_line, in_ecosite, out_line, processes, verb
         in_properties = in_file_vector.meta['schema']['properties']
         for line in in_file_vector:
             if line.geometry:
-                if line.geometry.type != 'MultiLineString':
+                if line.geometry.type != 'MultiLineString' or \
+                   line.geometry.type == 'Polygon' or \
+                   line.geometry.type == 'MultiPolygon':
                     input_lines.append([line.geometry, line.properties])
                 else:
                     print('MultiLineString found.')
@@ -65,7 +69,7 @@ def forest_line_ecosite(callback, in_line, in_ecosite, out_line, processes, verb
         index_query = tree.query(line_geom)
         geoms_intersected = []
         for i in index_query:
-            geoms_intersected.append({'geom': geoms[i], 'prop': feats[i][1]})  # polygon has property
+            geoms_intersected.append({'geom': feats[i][0], 'prop': feats[i][1]})  # polygon has property
         all_lines.append(({'geom': line_geom, 'prop': line_prop}, geoms_intersected, id))
         id += 1
 
@@ -100,6 +104,10 @@ def forest_line_ecosite(callback, in_line, in_ecosite, out_line, processes, verb
                 print(' %{} '.format(step / total_steps * 100), flush=True)
 
     i = 0
+    feat_type = None
+    if len(feat_all) > 0:
+        feat_type = type(feat_all[0][0][0])
+
     for feature in feat_all:
         if not feature:
             continue
@@ -116,8 +124,12 @@ def forest_line_ecosite(callback, in_line, in_ecosite, out_line, processes, verb
             else:
                 fiona_features.append(single_line)
 
+    geom_type = 'LineString'
+    if feat_type is Polygon or feat_type is MultiPolygon:
+        geom_type = 'Polygon'
+
     schema = {
-        'geometry': 'LineString',
+        'geometry': geom_type,
         'properties': out_fields_list
     }
 
@@ -146,6 +158,12 @@ def split_line_with_polygon(lines, polygon):
     return line_list
 
 
+def split_polygon_with_polygon(in_poly, polygon):
+    inter = in_poly.intersection(polygon)
+    if not inter.is_empty:
+        return inter
+
+
 # @profile
 def process_single_line(line_args, find_nearest=True, output_linear_reference=False):
     """
@@ -160,23 +178,29 @@ def process_single_line(line_args, find_nearest=True, output_linear_reference=Fa
         The return list consist of split lines by intersection with polygons
 
     """
-    in_geom = line_args[0]
-    polygons = line_args[1]
+    in_feat = line_args[0]
+    in_geom = in_feat['geom']
+    poly_feats = line_args[1]
 
-    out_geom = [in_geom['geom']]
-    if len(polygons) > 0:  # none intersecting polygons
-        for poly in polygons:
-            out_geom = split_line_with_polygon(out_geom, poly['geom'])
+    if type(in_geom) is LineString or type(in_geom) is MultiLineString:
+        out_geom = [in_geom]
+        if len(poly_feats) > 0:  # none intersecting polygons
+            for poly in poly_feats:
+                out_geom = split_line_with_polygon(out_geom, poly['geom'])
+    elif type(in_geom) is Polygon or type(in_geom) is MultiPolygon:
+        out_geom = []
+        if len(poly_feats) > 0:  # none intersecting polygons
+            for poly in poly_feats:
+                out_geom.append(split_polygon_with_polygon(in_geom, poly['geom']))
 
     final_geoms = []
     if len(out_geom) > 0:
         for i in out_geom:
-            temp_prop = in_geom['prop']
-            for j in polygons:
+            temp_prop = Properties.from_dict(dict(in_feat['prop'].items()))
+            for j in poly_feats:
                 if j['geom'].contains(i):
                     temp_prop['ecosite'] = j['prop']['ecosite']  # TODO: specify 'ecosite' field name
                     final_geoms.append([i, temp_prop])
-
 
     return final_geoms
 
