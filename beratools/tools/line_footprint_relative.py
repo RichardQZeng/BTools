1f111d2886fd4dc3442630e9397648455d6a4016
Maverick <87385184+Maverick-F@users.noreply.github.com>
2024-03-15 21:36:03 -0600

    Codes updated

--- beratools/tools/line_footprint_relative.py
+++ beratools/tools/line_footprint_relative.py
@@ -55,7 +55,6 @@ if __name__ == '__main__':
     args.input["in_line"] = dy_cl_line
     del args.input['off_ln_dist']
     del args.input['canopy_percentile']
-    del args.input['canopy_thresh_percentage']
     verbose = True if args.verbose == 'True' else False
     main_line_footprint_relative(print, **args.input, processes=int(args.processes), verbose=verbose)
 
