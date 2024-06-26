9b19ded7d957733ddfbab15e6746b97e033e3819
Richard <502763+RichardQZeng@users.noreply.github.com>
2024-06-26 14:43:22 -0600

    Build working

--- beratools/gui/__init__.py
+++ beratools/gui/__init__.py
@@ -1,6 +1,14 @@
 # -*- coding: utf-8 -*-
 
 """Top-level package for BERA Tools."""
+import os
+import sys
+import inspect
+from .bt_gui_main import *
+
+current_dir = os.path.dirname(os.path.abspath(inspect.getfile(inspect.currentframe())))
+parent_dir = os.path.dirname(current_dir)
+sys.path.insert(0, parent_dir)
 
 __author__ = """AppliedGRG"""
 __email__ = 'appliedgrg@gmail.com'
@@ -8,13 +16,7 @@ __version__ = '0.1'
 
 name = 'gui'
 
-import os
-import sys
-import inspect
-currentdir = os.path.dirname(os.path.abspath(inspect.getfile(inspect.currentframe())))
-parentdir = os.path.dirname(currentdir)
-sys.path.insert(0, parentdir)
 
-# from .BTools import *
-
+def gui_main():
+    runner()
 
