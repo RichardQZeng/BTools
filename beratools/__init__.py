12839c02f8b6c5d4440d7dc701d2cb0d1e8f7edf
Richard Zeng <502763+RichardQZeng@users.noreply.github.com>
2024-06-28 13:32:53 -0600

    Update __init__.py
    Import all tools

--- beratools/__init__.py
+++ beratools/__init__.py
@@ -1,19 +1,3 @@
-# -*- coding: utf-8 -*-
-
-"""Top-level package for BERA Tools."""
-import os
-import sys
-sys.path.append(os.path.dirname(os.path.realpath(__file__)))
-
-from bt_gui_main import *
-
-__author__ = """AppliedGRG"""
-__email__ = 'appliedgrg@gmail.com'
-__version__ = '0.1'
-
-name = 'gui'
-
-
-def gui_main():
-    runner()
+from beratools.tools import *
+from beratools.gui import *
 
