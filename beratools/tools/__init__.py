d9c2ec04dd7db0f182cf172e2fc9640a08078d11
Richard Zeng <502763+RichardQZeng@users.noreply.github.com>
2024-06-28 13:34:41 -0600

    Update __init__.py

--- beratools/tools/__init__.py
+++ beratools/tools/__init__.py
@@ -2,9 +2,6 @@
 
 import os
 import sys
-import inspect
-currentdir = os.path.dirname(os.path.abspath(inspect.getfile(inspect.currentframe())))
-parentdir = os.path.dirname(currentdir)
-sys.path.insert(0, parentdir)
+sys.path.append(os.path.dirname(os.path.realpath(__file__)))
 
 name = 'tools'
