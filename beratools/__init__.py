f639c3e2cfb59d7ff195367106593038f8a0cc6e
RichardQZeng <502763+RichardQZeng@users.noreply.github.com>
2024-09-14 03:08:42 +0000

    Fix issue for Linux.

--- beratools/__init__.py
+++ beratools/__init__.py
@@ -1,7 +1,6 @@
 from beratools.core import *
 from beratools.tools import *
 from beratools.gui import *
-from beratools.third_party import *
 from beratools.core import logger
 
 __author__ = """AppliedGRG"""
