f84b857f1ef184d56d2d76c6ab5e7b6c2bc428d4
Richard <502763+RichardQZeng@users.noreply.github.com>
2024-12-21 23:02:14 -0700

    Update full_workflow.py

--- notebooks/full_workflow.py
+++ notebooks/full_workflow.py
@@ -6,6 +6,7 @@ from inspect import getsourcefile
 
 if __name__ == "__main__":
     current_file = Path(getsourcefile(lambda: 0)).resolve()
+    current_folder = current_file.parent
     btool_dir = current_file.parents[1]
     sys.path.insert(0, btool_dir.as_posix())
 
@@ -23,26 +24,29 @@ processes = 18
 verbose = False
 
 if __name__ == '__main__':
+    if len(sys.argv) > 1:
+        processes = int(sys.argv[1])
+        print(f'CPU cores: {processes}')
 
-    with open(r"./params_hpc.yml") as in_params:
+    with open(current_folder.joinpath('params_win.yml')) as in_params:
         params = yaml.safe_load(in_params)
 
     # ### centerline
     args_centerline = params['args_centerline']
-    args_centerline
+    print(args_centerline)
 
     centerline(**args_centerline, processes=processes, verbose=verbose)
 
     # canopy footprint
     args_canopy_threshold = params["args_canopy_threshold"]
-    args_canopy_threshold
+    print(args_canopy_threshold)
 
     dy_cl_line = main_canopy_threshold_relative(
         callback=print, **args_canopy_threshold, processes=processes, verbose=verbose
     )
 
     args_line_footprint_relative = params["args_line_footprint_relative"]
-    args_line_footprint_relative
+    print(args_line_footprint_relative)
 
     main_line_footprint_relative(
         callback=print, **args_line_footprint_relative, processes=processes, verbose=verbose
@@ -50,7 +54,7 @@ if __name__ == '__main__':
     
     # ground footprint
     args_line_footprint_fixed = params["args_line_footprint_fixed"]
-    args_line_footprint_fixed
+    print(args_line_footprint_fixed)
 
     line_footprint_fixed(
         callback=print, **args_line_footprint_fixed, processes=processes, verbose=verbose
