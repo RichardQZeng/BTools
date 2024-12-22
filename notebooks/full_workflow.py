6cb70ae096744480dd950e601e70442b46e492df
Richard <502763+RichardQZeng@users.noreply.github.com>
2024-12-21 21:27:44 -0700

    Create full_workflow.py
    Working version for Windows

---
+++ notebooks/full_workflow.py
@@ -0,0 +1,57 @@
+import os
+
+import sys
+from pathlib import Path
+from inspect import getsourcefile
+
+if __name__ == "__main__":
+    current_file = Path(getsourcefile(lambda: 0)).resolve()
+    btool_dir = current_file.parents[1]
+    sys.path.insert(0, btool_dir.as_posix())
+
+from beratools.tools.centerline import centerline
+from beratools.core.algo_canopy_threshold_relative import main_canopy_threshold_relative
+from beratools.core.algo_line_footprint_functions import main_line_footprint_relative
+from beratools.tools.line_footprint_fixed import line_footprint_fixed
+
+import yaml
+
+gdal_env = os.environ.get("GDAL_DATA")
+gdal_env
+
+processes = 18
+verbose = False
+
+if __name__ == '__main__':
+
+    with open(r"./params_hpc.yml") as in_params:
+        params = yaml.safe_load(in_params)
+
+    # ### centerline
+    args_centerline = params['args_centerline']
+    args_centerline
+
+    centerline(**args_centerline, processes=processes, verbose=verbose)
+
+    # canopy footprint
+    args_canopy_threshold = params["args_canopy_threshold"]
+    args_canopy_threshold
+
+    dy_cl_line = main_canopy_threshold_relative(
+        callback=print, **args_canopy_threshold, processes=processes, verbose=verbose
+    )
+
+    args_line_footprint_relative = params["args_line_footprint_relative"]
+    args_line_footprint_relative
+
+    main_line_footprint_relative(
+        callback=print, **args_line_footprint_relative, processes=processes, verbose=verbose
+    )
+    
+    # ground footprint
+    args_line_footprint_fixed = params["args_line_footprint_fixed"]
+    args_line_footprint_fixed
+
+    line_footprint_fixed(
+        callback=print, **args_line_footprint_fixed, processes=processes, verbose=verbose
+    )
