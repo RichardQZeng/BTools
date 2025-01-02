5d2451f098c431f929d8ac4b5ef7584119c3d6c6
Richard Zeng <502763+RichardQZeng@users.noreply.github.com>
2025-01-02 12:58:21 -0700

    Update full workflow scripts

--- notebooks/full_workflow.py
+++ notebooks/full_workflow.py
@@ -13,8 +13,9 @@ if __name__ == "__main__":
     sys.path.insert(0, btool_dir.as_posix())
 
 from beratools.tools.centerline import centerline
-from beratools.core.algo_canopy_threshold_relative import main_canopy_threshold_relative
-from beratools.core.algo_line_footprint_functions import main_line_footprint_relative
+# from beratools.core.algo_canopy_threshold_relative import main_canopy_threshold_relative
+# from beratools.core.algo_line_footprint_functions import main_line_footprint_relative
+from notebooks.footprint_canopy import FootprintCanopy
 from beratools.tools.line_footprint_fixed import line_footprint_fixed
 
 import yaml
@@ -38,27 +39,37 @@ if __name__ == '__main__':
         params = yaml.safe_load(in_params)
 
     # centerline
-    # args_centerline = params['args_centerline']
-    # print(args_centerline)
-    # centerline(**args_centerline, processes=processes, verbose=verbose)
+    args_centerline = params['args_centerline']
+    print(args_centerline)
+    centerline(**args_centerline, processes=processes, verbose=verbose)
 
     # canopy footprint
-    args_canopy_threshold = params["args_canopy_threshold"]
-    print(args_canopy_threshold)
-    dy_cl_line = main_canopy_threshold_relative(
-        callback=print, **args_canopy_threshold, processes=processes, verbose=verbose
-    )
+    # args_canopy_threshold = params["args_canopy_threshold"]
+    # print(args_canopy_threshold)
+    # dy_cl_line = main_canopy_threshold_relative(
+    #     callback=print, **args_canopy_threshold, processes=processes, verbose=verbose
+    # )
 
-    args_line_footprint_relative = params["args_line_footprint_relative"]
-    print(args_line_footprint_relative)
-    main_line_footprint_relative(
-        callback=print, **args_line_footprint_relative, processes=processes, verbose=verbose
-    )
+    # args_line_footprint_relative = params["args_line_footprint_relative"]
+    # print(args_line_footprint_relative)
+    # main_line_footprint_relative(
+    #     callback=print, **args_line_footprint_relative, processes=processes, verbose=verbose
+    # )
+
+    # alternative relative footprint
+    fp_params = params['args_footprint_canopy']
+    in_file = fp_params['in_file']
+    in_chm = fp_params["in_chm"]
+    out_file_percentile = fp_params["out_file_percentile"]
+    out_file_fp = fp_params["out_file_fp"]
+
+    footprint = FootprintCanopy(in_file, in_chm)
+    footprint.compute()
 
     # ground footprint
-    # args_line_footprint_fixed = params["args_line_footprint_fixed"]
-    # print(args_line_footprint_fixed)
+    args_line_footprint_fixed = params["args_line_footprint_fixed"]
+    print(args_line_footprint_fixed)
 
-    # line_footprint_fixed(
-    #     callback=print, **args_line_footprint_fixed, processes=processes, verbose=verbose
-    # )
+    line_footprint_fixed(
+        callback=print, **args_line_footprint_fixed, processes=processes, verbose=verbose
+    )
