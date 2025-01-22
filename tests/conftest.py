d8dd947df1907cecb42991279342028759a5f9d9
Richard <502763+RichardQZeng@users.noreply.github.com>
2025-01-22 13:01:37 -0700

    Update conftest.py

--- tests/conftest.py
+++ tests/conftest.py
@@ -1,12 +1,18 @@
-import fiona
+"""
+Test file.
+
+Returns:
+    _type_: _description_
+
+"""
+import geopandas as gpd
 import os
 import pytest
-from shapely.geometry import shape
 
 TESTDATA_DIR = os.path.join(os.path.dirname(os.path.realpath(__file__)), "testdata")
 
 
 @pytest.fixture
 def alps_shape():
-    with fiona.open(os.path.join(TESTDATA_DIR, "alps.geojson"), "r") as src:
-        return shape(next(iter(src))["geometry"])
+    gdf = gpd.read_file(os.path.join(TESTDATA_DIR, "alps.shp"))
+    return gdf
