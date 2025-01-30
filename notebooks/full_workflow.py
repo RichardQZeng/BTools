a77ca9e544cb5a5620b4852a28993b4e9856ed54
Richard <502763+RichardQZeng@users.noreply.github.com>
2025-01-29 18:32:35 -0700

    Update full_workflow.py
    Retrieve OS and CPU cores to reduce command line args.

--- notebooks/full_workflow.py
+++ notebooks/full_workflow.py
@@ -2,15 +2,16 @@
 Provide a full workflow that runs the centerline, canopy and ground footprint tools.
 
 usage: 
-    python full_workflow.py -c 10 -p win/hpc -f some.yml
+    python full_workflow.py [-f some.yml] [-m   PARALLEL_MODE]
 """
 
 import argparse
 import os
+import platform
+import sys
 from pathlib import Path
 from pprint import pprint
 
-import sys
 sys.path.append(Path(__file__).resolve().parents[1].as_posix())
 
 import yaml
@@ -24,30 +25,13 @@ from beratools.tools.line_footprint_fixed import line_footprint_fixed
 print = pprint
 
 gdal_env = os.environ.get("GDAL_DATA")
-gdal_env
 
-processes = 12
+processes = 2
 verbose = False
 
 def check_arguments():
     # Create the argument parser
     parser = argparse.ArgumentParser(description="Run the full workflow parameters.")
-    
-    # Add arguments for each parameter
-    parser.add_argument(
-        '-p', '--platform', 
-        choices=['win', 'hpc'], 
-        required=True, 
-        default="hpc",
-        help="Specify the platform: 'win' or 'hpc'"
-    )
-    
-    parser.add_argument(
-        '-c', '--cores', 
-        type=int, 
-        required=True, 
-        help="Number of cores to allocate (integer)"
-    )
     
     # Make the YAML file optional by setting required=False
     parser.add_argument(
@@ -76,58 +60,57 @@ if __name__ == '__main__':
     script_dir = Path(__file__).parent
 
     args = check_arguments()
+    platform = platform.system()
 
     # Access the parameters
-    platform = args.platform
-    cores = args.cores
-    file = args.file
+    yml_file = args.file
 
     # Set default file based on platform if not provided
-    if not file:
-        if platform == 'hpc':
-            file = script_dir / 'params_hpc.yml'  # Use pathlib to join paths
+    if not yml_file:
+        if platform == 'Linux':
+            yml_file = script_dir / 'params_hpc.yml'  # Use pathlib to join paths
         elif platform == 'win':
-            file = script_dir / 'params_win.yml'
+            yml_file = script_dir / 'params_win.yml'
+
+    # Get available CPU cores
+    processes = os.cpu_count()
 
     # Print the received arguments (you can replace this with actual processing code)
     print(f"Platform: {platform}")
-    print(f"Cores: {cores}")
-    if file:
-        print(f"Configuration file: {file}")
-    else:
-        print("No configuration file provided.")
-
-    if args.cores:
-        processes = args.cores
-        print(f'CPU cores: {processes}')
-
+    print(f"Cores: {processes}")
     print(f'Parallel mode: {PARALLEL_MODE.name}')
-
-    yml_file = Path(__file__).parent.joinpath("params_" + platform).with_suffix(".yml")
-    print(f"Config file: {yml_file}")
+    print(f"Configuration file: {yml_file}")
 
     with open(yml_file) as in_params:
         params = yaml.safe_load(in_params)
 
     # centerline
+    print("Starting centerline")
+    print('-'*50)
     args_centerline = params['args_centerline']
     args_centerline['processes'] = processes
     print(args_centerline)
     centerline(**args_centerline)
 
     # canopy footprint abs
+    print("Starting canopy footprint abs")
+    print('-'*50)
     args_footprint_abs = params["args_footprint_abs"]
     args_footprint_abs['processes'] = processes
     print(args_footprint_abs)
     line_footprint_abs(**args_footprint_abs)
 
     # canopy footprint relative
+    print("Starting canopy footprint rel")
+    print('-'*50)
     args_footprint_rel = params["args_footprint_rel"]
     args_footprint_rel['processes'] = processes
     print(args_footprint_rel)
     line_footprint_rel(**args_footprint_rel)
 
     # ground footprint
+    print("Starting ground footprint")
+    print('-'*50)
     args_line_footprint_fixed = params["args_line_footprint_fixed"]
     args_line_footprint_fixed['processes'] = processes
     print(args_line_footprint_fixed)
