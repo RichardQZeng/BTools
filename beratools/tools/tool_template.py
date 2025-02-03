9906a4d16ce94a1f60d2840e84be152ae8038c39
Richard <502763+RichardQZeng@users.noreply.github.com>
2025-02-03 10:51:21 -0700

    Update tool_template.py

--- beratools/tools/tool_template.py
+++ beratools/tools/tool_template.py
@@ -1,21 +1,57 @@
+"""
+Copyright (C) 2025 Applied Geospatial Research Group.
+
+This script is licensed under the GNU General Public License v3.0.
+See <https://gnu.org/licenses/gpl-3.0> for full license details.
+
+Author: AUTHOR NAME
+
+Description:
+    This script is part of the BERA Tools.
+    Webpage: https://github.com/appliedgrg/beratools
+
+    The purpose of this script is to provide template for tool.
+"""
 import time
 from multiprocessing.pool import Pool
 from random import random
 
-from common import *
-from numpy import mean
+import numpy as np
 
+import beratools.tools.common as bt_common
+from beratools.core.tool_base import execute_multiprocessing
 
-def tool_name(callback, in_line, in_cost_raster, line_radius, process_segments, out_center_line):
-    execute_multiprocessing()
-    callback('tool_template tool done.')
+# Example task_data as a list of numpy ndarrays
+task_data_list = [
+    np.array([1, 2, 3]),
+    np.array([4, 5, 6]),
+    np.array([7, 8, 9])
+]
+
+def tool_name(
+    in_line, in_cost_raster, line_radius, out_line, processes, verbose
+):
+    """
+    Define tool entry point.
+    
+    These arguments are defined in beratools.json file. 
+    execute_multiprocessing is common function to run tasks in parallel.
+    """
+    result = execute_multiprocessing(
+        worker,
+        task_data_list,
+        "tool_template",
+        processes,
+        verbose=verbose
+    )
+    print(len(result))
 
 
 # task executed in a worker process
 def worker(task_data):
     # report a message
-    value = mean(task_data)
-    print(f'Task {len(task_data)} with {value} executed', flush=True)
+    value = np.mean(task_data)
+    print(f'Task with {value} executed', flush=True)
 
     # block for a moment
     time.sleep(value * 10)
@@ -24,23 +60,10 @@ def worker(task_data):
     return value
 
 
-# protect the entry point
-def execute_multiprocessing():
-    # create and configure the process pool
-    data = [[random() for n in range(100)] for i in range(300)]
-    total_steps = 300
-    with Pool() as pool:
-        step = 0
-        # execute tasks in order, process results out of order
-        for result in pool.imap_unordered(worker, data):
-            print(f'Got result: {result}', flush=True)
-            step += 1
-            print(step)
-            print('%{}'.format(step / total_steps * 100))
-
-
 if __name__ == '__main__':
-    in_args, in_verbose = check_arguments()
+    in_args, in_verbose = bt_common.check_arguments()
     start_time = time.time()
-    tool_name(print, **in_args.input, processes=int(in_args.processes), verbose=in_verbose)
-    print('Elapsed time: {}'.format(time.time() - start_time))
+    tool_name(
+        print, **in_args.input, processes=int(in_args.processes), verbose=in_verbose
+    )
+    print("Elapsed time: {}".format(time.time() - start_time))
